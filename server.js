e1dd37cc013c629d6836cae242cbae0dc522f28f
OrelTwito <twitoorel1@gmail.com>
2022-12-03 09:09:38 +0200

    Connect MongoDB Atlas

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const database_endpoint = "mongodb://localhost:27017/RestApi-Store";
+// const database_endpoint = "mongodb://localhost:27017/RestApi-Store";
+const database_endpoint =
+  "mongodb+srv://twitoorel1:Ot2521ot@cluster0.pu4qk.mongodb.net/RestApi-Store";
+
 mongoose
   .connect(database_endpoint)
   .then(() => console.log("💻 MongoDB Connected"))
