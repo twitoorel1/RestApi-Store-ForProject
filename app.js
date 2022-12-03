b337c3698e7c33cd2de7d1d724c798e883f578b4
OrelTwito <twitoorel1@gmail.com>
2022-12-03 10:57:20 +0200

    all

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ const AppError = require("./utils/AppError");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  req.header("Content-Type", "application/json");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.set(
-    "Cache-Control",
-    "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   req.header("Content-Type", "application/json");
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+//   res.set(
+//     "Cache-Control",
+//     "no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0"
+//   );
+//   next();
+// });
 
 app.get("/", (req, res) => {
   res.send("Home Page RestApi");
